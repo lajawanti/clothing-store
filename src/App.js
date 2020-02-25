4648c72cabc5fe13e412c396a23c1bad0b863c46
lajawanti <lajawanti.dhake@gmail.com>
2020-02-25 14:11:30 -0500

    HomePage with five menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './HomePage.component.jsx'
+
 function App() {
   return (
     <div className="App">
       <h1>Clothing Store....</h1>
+      <HomePage />
     </div>
   );
 }
