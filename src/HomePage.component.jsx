cc780baf0f5827d09d8cb761b72ad433243afc51
lajawanti <lajawanti.dhake@gmail.com>
2020-02-25 14:30:48 -0500

    SCSS add

--- src/HomePage.component.jsx
+++ src/HomePage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homepage.styles.scss'
 
 const HomePage = () => (
     <div className = 'homepage'>
@@ -38,12 +39,6 @@ const HomePage = () => (
                 </div>
             </div>
 
-            <div className = 'menu-item'>
-                <div className = 'content'>
-                    <h1>HATS</h1>
-                    <span>SHOP NOW</span>
-                </div>
-            </div>
         </div>
     </div>
 )
