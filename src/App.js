cd8483e2481fb957e92edd24701fed2aee11693b
lajawanti <lajawanti.dhake@gmail.com>
2020-02-28 17:08:00 -0500

    configuring react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx'
 
 function App() {
   return (
     <div className="App">
+
       <h1>Clothing Store....</h1>
+      
       <HomePage />
     </div>
   );
