b10267ddcc32b96cf8f3fe87b2d4d5e2b5ffea23
lajawanti <lajawanti.dhake@gmail.com>
2020-03-03 14:22:17 -0500

    sign-in in progress

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUp = () => (
+    <div class = 'sign-in-and-sign-up'>
+        SIGN IN
+    </div>
+)
+
+export default SignInAndSignUp;
