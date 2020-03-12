7cb36a4f1b0966f1a9f96b20758a7b1cf8e01442
lajawanti <lajawanti.dhake@gmail.com>
2020-03-12 13:35:44 -0400

    Adding user to firebase database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
-import { auth } from './firebase/firebase.utils';
+import { createUserProfileDocument,  auth } from './firebase/firebase.utils';
 
 class App extends React.Component {
   state = {
@@ -17,8 +17,9 @@ class App extends React.Component {
 
   componentDidMount() {
       this. unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-        this.setState({ currentUser : user});
-        console.log(user);
+        // this.setState({ currentUser : user});
+        // console.log(user);
+        createUserProfileDocument(user);
       })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,11 +14,36 @@ const config = {
         measurementId: "G-Q5QFEPX2D2"
 };
 
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+        if(!userAuth) return;
+
+        const userRef = firestore.doc(`users/${userAuth.uid}`);
+        const snapShot = await userRef.get();
+
+        if(!snapShot.exists) { //userRef returns from firestore doc a documentReferenec which tells exists (true/false if user already in database)
+                //if user does not exists create by using set() method 
+                const { displayName, email } = userAuth;
+                const createAt = new Date();
+
+                try {
+                        await userRef.set({
+                                displayName,
+                                email,
+                                createAt,
+                                ...additionalData
+                        })
+                } catch(error) {
+                        console.log('error creating user', error.message)
+                }
+        }
+        return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
+//Sign-in through GoogleAuthProvider
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
