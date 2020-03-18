8ec872379ad733cb51fb234a8c4368db9397bd11
lajawanti <lajawanti.dhake@gmail.com>
2020-03-17 23:36:47 -0400

    rootReducer with combineReducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,2 +1,9 @@
 //All reducer should go in root-reducer
 
+import { combineReducers } from 'redux';
+
+import userReducer from './user/user.reducer.jsx';
+
+export default combineReducers({
+    user: userReducer
+});
